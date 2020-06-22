663c7786f6b013d8585bc4f63f6c6750777e54d5
JACK <zhenya271@gmail.com>
2020-06-22 23:18:54 +0300

    Initial commit

---
+++ precache-manifest.b93e25841f3bdf3a87c0472c972cd09e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac94a1c343315788d666c744417413fa",
+    "url": "/WeatherAppWithFavourite/index.html"
+  },
+  {
+    "revision": "2abec8e5dcfd8f66405f",
+    "url": "/WeatherAppWithFavourite/static/css/main.24a18e7e.chunk.css"
+  },
+  {
+    "revision": "c2a3c538d03ee69aecf1",
+    "url": "/WeatherAppWithFavourite/static/js/2.0dadff8d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/WeatherAppWithFavourite/static/js/2.0dadff8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2abec8e5dcfd8f66405f",
+    "url": "/WeatherAppWithFavourite/static/js/main.226ef550.chunk.js"
+  },
+  {
+    "revision": "5f8feabebde33c809092",
+    "url": "/WeatherAppWithFavourite/static/js/runtime-main.e78daece.js"
+  }
+]);
